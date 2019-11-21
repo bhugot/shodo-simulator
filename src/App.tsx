dbdddc58e22824c9856674dc6c425cbdcd211870
Benjamin Hugot <bhugot@gmail.com>
2019-11-21 17:44:18 +0100

    aaaaand it's still working

--- src/App.tsx
+++ src/App.tsx
@@ -15,30 +15,31 @@ const salaryGrid: Map<number, number> = new Map<number, number>([
   [9, 70000]
 ]);
 
-class SimulatorShodo extends React.Component<{}, { experience: string, salary?: number }> {
+class SimulatorShodo extends React.Component<{}, { experience: number, salary?: number }> {
   constructor(props: any) {
     super(props);
-    this.state = { experience: '0', salary: 0 }
+    this.state = { experience: 0, salary: salaryGrid.get(0) };
+  
   }
 
-  updateSalary(event: any) {
-    this.setState({ experience: event.target.value, salary: salaryGrid.get(Number.parseInt(event.target.value)) })
+  updateSalary(experience: number) {
+    this.setState({ experience, salary: salaryGrid.get(experience)});
   }
 
   render() {
     return (
       <div className="App">
-        <select value={this.state.experience} onChange={ (event) => this.updateSalary(event)}>
-          <option value="0">Noob</option>
-          <option value="1">1</option>
-          <option value="2">2</option>
-          <option value="3">3</option>
-          <option value="4">4</option>
-          <option value="5">5</option>
-          <option value="6">6</option>
-          <option value="7">7</option>
-          <option value="8">8</option>
-          <option value="9">9 et +</option>
+        <select value={this.state.experience} onChange={ (event) => this.updateSalary(Number.parseInt(event.target.value))}>
+          <option value={0}>Noob</option>
+          <option value={1}>1</option>
+          <option value={2}>2</option>
+          <option value={3}>3</option>
+          <option value={4}>4</option>
+          <option value={5}>5</option>
+          <option value={6}>6</option>
+          <option value={7}>7</option>
+          <option value={8}>8</option>
+          <option value={9}>9 et +</option>
         </select>
 
         <br />
